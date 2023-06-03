9ba3eaa68e112ccc98dc5cd5d55f5fb9bf7462d9
hagar110 <hagar.ehab999@gmail.com>
2023-06-03 15:44:49 +0300

    adds additional fields to the meter annotation

--- src/main/java/com/example/monitoringlib/annotations/Meter.java
+++ src/main/java/com/example/monitoringlib/annotations/Meter.java
@@ -1,5 +1,6 @@
 package com.example.monitoringlib.annotations;
 
+import com.example.monitoringlib.enums.MeterType;
 import com.example.monitoringlib.enums.RecordingTime;
 
 import java.lang.annotation.ElementType;
@@ -10,8 +11,11 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Meter {
-   RecordingTime recordingTime() default RecordingTime.BEFORE;
+    RecordingTime recordingTime() default RecordingTime.BEFORE;
+
     Value value() default @Value();
 
-    String name() default "";;
+    String name() default "";
+    MeterType meterType() default MeterType.GAUGE;
+    Tag[] tags();
 }
