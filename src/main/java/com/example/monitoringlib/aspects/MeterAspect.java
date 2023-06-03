cd70656762eaf6b15fe6aa1d670760c310da8fc6
hagar110 <hagar.ehab999@gmail.com>
2023-06-03 18:11:21 +0300

    adds error recording time

--- src/main/java/com/example/monitoringlib/aspects/MeterAspect.java
+++ src/main/java/com/example/monitoringlib/aspects/MeterAspect.java
@@ -21,6 +21,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
@@ -37,7 +38,7 @@ public class MeterAspect {
     private MeterRegistry meterRegistry;
 
     @Before(value = "@annotation(com.example.monitoringlib.annotations.Meter)")
-    public void logMethodExecutionInfo(JoinPoint joinPoint)  {
+    public void logMethodExecutionInfo(JoinPoint joinPoint) {
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
         logger.info("full method description:{} ", signature.getMethod());
         logger.info("method name: {}", signature.getMethod().getName());
@@ -58,7 +59,7 @@ public class MeterAspect {
         }
         MethodSignature methodSignature = (MethodSignature) signature;
         List<Meter> annotations = getAnnotations(Meter.class, methodSignature.getMethod());
-        processAnnotations(annotations,joinPoint,methodSignature.getMethod());
+        processAnnotations(annotations, joinPoint, methodSignature.getMethod());
 
     }
 
@@ -80,55 +81,63 @@ public class MeterAspect {
         Parameter[] parameters = method.getParameters();
         Object[] args = joinPoint.getArgs();
         Map<String, Object> argsMap = getMethodArguments(parameters, args);
-        annotations.stream().filter(annotation -> annotation.recordingTime() == RecordingTime.BEFORE).forEach(e -> recordMeter((Meter) e,argsMap,null,-1L));//duration =--1 as the method execution not yet started
-        long startInNanos=System.nanoTime();
-        Object outcome= joinPoint.proceed();
-        long durationInNanos=System.nanoTime()-startInNanos;
-        annotations.stream().filter(annotation -> annotation.recordingTime() == RecordingTime.AFTER).forEach(e -> recordMeter((Meter) e,argsMap,outcome,durationInNanos));
+        annotations.stream().filter(annotation -> annotation.recordingTime() == RecordingTime.BEFORE).forEach(e -> recordMeter((Meter) e, argsMap, null, -1L));//duration =--1 as the method execution not yet started
+        long startInNanos = System.nanoTime();
+        Object outcome = null;
+        try {
+            outcome = joinPoint.proceed();
+        } catch (Exception ex) {
+            annotations.stream().filter(annotation -> annotation.recordingTime() == RecordingTime.ERROR).forEach(e -> recordMeter((Meter) e, argsMap, ex, Long.MAX_VALUE));
         }
+        if (outcome != null) {
+            long durationInNanos = System.nanoTime() - startInNanos;
+            Object finalOutcome = outcome;
+            annotations.stream().filter(annotation -> annotation.recordingTime() == RecordingTime.AFTER).forEach(e -> recordMeter((Meter) e, argsMap, finalOutcome, durationInNanos));
+        }
+    }
 
-    public void recordMeter(Meter meter,Map<String,Object> argsMap,Object outcome,long duration) {
+    public void recordMeter(Meter meter, Map<String, Object> argsMap, Object outcome, long duration) {
 
-        Optional<Object> gaugeValue = getValue(meter.value(),argsMap,outcome);
+        Optional<Object> gaugeValue = getValue(meter.value(), argsMap, outcome);
         Tag[] tags = meter.tags();
         String meterName = meter.name();
 
         switch (meter.meterType()) {
             case GAUGE:
-                recordGauge(meterName,tags,toDoubleValue(gaugeValue.isEmpty()?null:gaugeValue.get()),argsMap,outcome);
+                recordGauge(meterName, tags, toDoubleValue(gaugeValue.isEmpty() ? null : gaugeValue.get()), argsMap, outcome);
                 break;
             case TIMER:
-                recordTimer(meterName,tags,argsMap,outcome,duration);
+                recordTimer(meterName, tags, argsMap, outcome, duration);
                 break;
             case COUNTER:
-                recordCounter(meterName,tags,argsMap,outcome);
+                recordCounter(meterName, tags, argsMap, outcome);
         }
 
 
         // meterRegistry.
     }
-    private double toDoubleValue(Object gaugeValue){
-        if(gaugeValue==null)
+
+    private double toDoubleValue(Object gaugeValue) {
+        if (gaugeValue == null)
             return 0d;
-        if(gaugeValue instanceof Number){
+        if (gaugeValue instanceof Number) {
             return (double) gaugeValue;
         }
-        if(gaugeValue instanceof String){
+        if (gaugeValue instanceof String) {
             try {
                 double val = Double.parseDouble((String) gaugeValue);
                 return val;
-            }
-            catch(Exception e){
-                logger.error("exception {} is thrown while parsing string to double",e);
+            } catch (Exception e) {
+                logger.error("exception {} is thrown while parsing string to double", e);
             }
         }
         return 0d;
     }
 
-    private Optional<Object> getValue(Value value, Map<String, Object> argsMap,Object outcome) {
+    private Optional<Object> getValue(Value value, Map<String, Object> argsMap, Object outcome) {
 
         Source source = value.source();
-        String valueName= value.name();
+        String valueName = value.name();
 
         switch (source) {
             case ARGUMENT:
@@ -140,26 +149,30 @@ public class MeterAspect {
             case CLOCK:
                 return Optional.of(LocalTime.now().toString());
             case OUTCOME:
-              return Optional.ofNullable(outcome);
+                return Optional.ofNullable(outcome);
         }
         return Optional.empty();
     }
+
     void recordTimer(String meterName, Tag[] tags, Map<String, Object> argsMap, Object outcome, long duration) {
         Timer timer = null;
-        timer=Timer.builder(meterName).tags(toMeterTag(tags,argsMap,outcome)).register(meterRegistry);
-        timer.record(()->duration);
+        timer = Timer.builder(meterName).tags(toMeterTag(tags, argsMap, outcome)).register(meterRegistry);
+        timer.record(() -> duration);
     }
-    void recordCounter(String meterName , Tag[] tags,Map<String, Object> argsMap, Object outcome) {
+
+    void recordCounter(String meterName, Tag[] tags, Map<String, Object> argsMap, Object outcome) {
         Counter counter = null;
-        counter=Counter.builder(meterName).tags(toMeterTag(tags,argsMap,outcome)).register(meterRegistry);
+        counter = Counter.builder(meterName).tags(toMeterTag(tags, argsMap, outcome)).register(meterRegistry);
         counter.increment();
     }
+
     void recordGauge(String meterName, Tag[] tags, double doubleValue, Map<String, Object> argsMap, Object outcome) {
         Gauge gauge = null;
-        gauge=Gauge.builder(meterName,()->doubleValue).tags(toMeterTag(tags,argsMap,outcome)).register(meterRegistry);
+        gauge = Gauge.builder(meterName, () -> doubleValue).tags(toMeterTag(tags, argsMap, outcome)).register(meterRegistry);
     }
-    private List<io.micrometer.core.instrument.Tag> toMeterTag(Tag[] tags,Map<String, Object> argsMap,Object outcome){
-        return Arrays.stream(tags).map(e->io.micrometer.core.instrument.Tag.of(e.name(),getValue(e.value(),argsMap,outcome).map(
+
+    private List<io.micrometer.core.instrument.Tag> toMeterTag(Tag[] tags, Map<String, Object> argsMap, Object outcome) {
+        return Arrays.stream(tags).map(e -> io.micrometer.core.instrument.Tag.of(e.name(), getValue(e.value(), argsMap, outcome).map(
                 String::valueOf).orElse(""))).collect(Collectors.toList());
     }
 }
